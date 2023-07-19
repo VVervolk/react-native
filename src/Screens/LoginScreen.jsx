08243e9c2c93f05dc6aa31c3596f03babc16d4b2
VVervolk <vervolk1234@gmail.com>
2023-07-19 17:51:12 +0300

    complete 3 hw

--- src/Screens/LoginScreen.jsx
+++ src/Screens/LoginScreen.jsx
@@ -1,101 +1,150 @@
 import {
   ImageBackground,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
   Pressable,
   StyleSheet,
   Text,
   TextInput,
+  TouchableWithoutFeedback,
   View,
 } from "react-native";
 import Background from "../images/background.jpg";
 import { styles } from "./RegistrationScreen";
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "input_email": {
+      return {
+        ...state,
+        email: action.email,
+      };
+    }
+    case "input_password": {
+      return {
+        ...state,
+        password: action.password,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
 
 export default function LoginScreen() {
   const [isPressed, setIsPressed] = useState(false);
   const [isFocusedEmail, setIsFocusedEmail] = useState(false);
   const [isFocusedPass, setIsFocusedPass] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    email: null,
+    password: null,
+  });
+
+  const onLog = () => console.log(state);
 
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={Background}
-        resizeMode="cover"
-        style={styles.image}
-      >
-        <View
-          style={[styles.substrate, { paddingTop: 32, paddingBottom: 144 }]}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={Background}
+          resizeMode="cover"
+          style={styles.image}
         >
-          <Text style={styles.title}>Увійти</Text>
-          <View style={styles.form}>
-            <TextInput
-              style={[
-                styles.input,
-                {
-                  borderColor: isFocusedEmail ? "#FF6C00" : "#E8E8E8",
-                  backgroundColor: isFocusedEmail ? "white" : "#F6F6F6",
-                },
-              ]}
-              inputMode="email"
-              placeholder="Адреса електронної пошти"
-              onFocus={() => setIsFocusedEmail(true)}
-              onBlur={() => setIsFocusedEmail(false)}
-            />
-
-            <View>
-              <TextInput
-                inputMode="text"
-                style={[
-                  styles.input,
-                  {
-                    borderColor: isFocusedPass ? "#FF6C00" : "#E8E8E8",
-                    backgroundColor: isFocusedPass ? "white" : "#F6F6F6",
-                  },
-                ]}
-                placeholder="Пароль"
-                secureTextEntry={true}
-                onFocus={() => setIsFocusedPass(true)}
-                onBlur={() => setIsFocusedPass(false)}
-              />
-              <Pressable style={styles.passwordButton}>
-                <Text style={styles.passwordButtonText}>Показати</Text>
-              </Pressable>
-            </View>
-          </View>
-          <Pressable
-            style={({ pressed }) => [
-              {
-                backgroundColor: pressed ? "white" : "#FF6C00",
-                borderColor: pressed ? "#FF6C00" : "white",
-              },
-              styles.submitButton,
-            ]}
-            onPressIn={() => {
-              setIsPressed(true);
-            }}
-            onPressOut={() => {
-              setIsPressed(false);
-            }}
+          <View
+            style={[styles.substrate, { paddingTop: 32, paddingBottom: 144 }]}
           >
-            <Text
-              style={[
-                styles.submitButtonText,
-                { color: isPressed ? "#FF6C00" : "white" },
+            <Text style={styles.title}>Увійти</Text>
+            <KeyboardAvoidingView
+              behavior={Platform.OS == "ios" ? "padding" : "height"}
+            >
+              <View style={styles.form}>
+                <TextInput
+                  value={state.email}
+                  onChangeText={(e) =>
+                    dispatch({ type: "input_email", email: e })
+                  }
+                  style={[
+                    styles.input,
+                    {
+                      borderColor: isFocusedEmail ? "#FF6C00" : "#E8E8E8",
+                      backgroundColor: isFocusedEmail ? "white" : "#F6F6F6",
+                    },
+                  ]}
+                  inputMode="email"
+                  placeholder="Адреса електронної пошти"
+                  onFocus={() => setIsFocusedEmail(true)}
+                  onBlur={() => setIsFocusedEmail(false)}
+                />
+
+                <View>
+                  <TextInput
+                    value={state.password}
+                    onChangeText={(e) =>
+                      dispatch({ type: "input_password", password: e })
+                    }
+                    inputMode="text"
+                    style={[
+                      styles.input,
+                      {
+                        borderColor: isFocusedPass ? "#FF6C00" : "#E8E8E8",
+                        backgroundColor: isFocusedPass ? "white" : "#F6F6F6",
+                      },
+                    ]}
+                    placeholder="Пароль"
+                    secureTextEntry={true}
+                    onFocus={() => setIsFocusedPass(true)}
+                    onBlur={() => setIsFocusedPass(false)}
+                  />
+                  <Pressable style={styles.passwordButton}>
+                    <Text style={styles.passwordButtonText}>Показати</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </KeyboardAvoidingView>
+
+            <Pressable
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed ? "white" : "#FF6C00",
+                  borderColor: pressed ? "#FF6C00" : "white",
+                },
+                styles.submitButton,
               ]}
+              onPressIn={() => {
+                setIsPressed(true);
+              }}
+              onPressOut={() => {
+                setIsPressed(false);
+              }}
+              onPress={onLog}
             >
-              Увійти
-            </Text>
-          </Pressable>
-          <Pressable style={loginStyles.loginLink}>
-            <Text style={styles.loginLinkText}>Немає акаунту?</Text>
-            <Text
-              style={[styles.loginLinkText, loginStyles.loginLinkTextUnderline]}
-            >
-              Зареєструватися
-            </Text>
-          </Pressable>
-        </View>
-      </ImageBackground>
-    </View>
+              <Text
+                style={[
+                  styles.submitButtonText,
+                  { color: isPressed ? "#FF6C00" : "white" },
+                ]}
+              >
+                Увійти
+              </Text>
+            </Pressable>
+            <Pressable style={loginStyles.loginLink}>
+              <Text style={styles.loginLinkText}>Немає акаунту?</Text>
+              <Text
+                style={[
+                  styles.loginLinkText,
+                  loginStyles.loginLinkTextUnderline,
+                ]}
+              >
+                Зареєструватися
+              </Text>
+            </Pressable>
+          </View>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
--- src/Screens/RegistrationScreen.jsx
+++ src/Screens/RegistrationScreen.jsx
@@ -1,112 +1,168 @@
 import {
   ImageBackground,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
   Pressable,
   StyleSheet,
   Text,
   TextInput,
+  TouchableWithoutFeedback,
   View,
 } from "react-native";
 import Background from "../images/background.jpg";
 import { AntDesign } from "@expo/vector-icons";
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "input_login": {
+      return {
+        ...state,
+        login: action.login,
+      };
+    }
+    case "input_email": {
+      return {
+        ...state,
+        email: action.email,
+      };
+    }
+    case "input_password": {
+      return {
+        ...state,
+        password: action.password,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
 
 export default function RegistrationScreen() {
   const [isPressed, setIsPressed] = useState(false);
   const [isFocusedLogin, setIsFocusedLogin] = useState(false);
   const [isFocusedEmail, setIsFocusedEmail] = useState(false);
   const [isFocusedPass, setIsFocusedPass] = useState(false);
+  const [state, dispatch] = useReducer(reducer, {
+    login: null,
+    email: null,
+    password: null,
+  });
+
+  const onReg = () => console.log(state);
 
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={Background}
-        resizeMode="cover"
-        style={styles.image}
-      >
-        <View style={styles.substrate}>
-          <View style={styles.avatar}>
-            <Pressable style={styles.addAvatarButton}>
-              <AntDesign name="pluscircleo" size={25} color="#FF6C00" />
-            </Pressable>
-          </View>
-          <Text style={styles.title}>Реєстрація</Text>
-          <View style={styles.form}>
-            <TextInput
-              inputMode="text"
-              style={[
-                styles.input,
-                {
-                  borderColor: isFocusedLogin ? "#FF6C00" : "#E8E8E8",
-                  backgroundColor: isFocusedLogin ? "white" : "#F6F6F6",
-                },
-              ]}
-              placeholder="Логін"
-              onFocus={() => setIsFocusedLogin(true)}
-              onBlur={() => setIsFocusedLogin(false)}
-            />
-            <TextInput
-              style={[
-                styles.input,
-                {
-                  borderColor: isFocusedEmail ? "#FF6C00" : "#E8E8E8",
-                  backgroundColor: isFocusedEmail ? "white" : "#F6F6F6",
-                },
-              ]}
-              inputMode="email"
-              placeholder="Адреса електронної пошти"
-              onFocus={() => setIsFocusedEmail(true)}
-              onBlur={() => setIsFocusedEmail(false)}
-            />
-            <View>
-              <TextInput
-                inputMode="text"
-                style={[
-                  styles.input,
-                  {
-                    borderColor: isFocusedPass ? "#FF6C00" : "#E8E8E8",
-                    backgroundColor: isFocusedPass ? "white" : "#F6F6F6",
-                  },
-                ]}
-                placeholder="Пароль"
-                secureTextEntry={true}
-                onFocus={() => setIsFocusedPass(true)}
-                onBlur={() => setIsFocusedPass(false)}
-              />
-              <Pressable style={styles.passwordButton}>
-                <Text style={styles.passwordButtonText}>Показати</Text>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <ImageBackground
+          source={Background}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <View style={styles.substrate}>
+            <View style={styles.avatar}>
+              <Pressable style={styles.addAvatarButton}>
+                <AntDesign name="pluscircleo" size={25} color="#FF6C00" />
               </Pressable>
             </View>
-          </View>
-          <Pressable
-            style={({ pressed }) => [
-              {
-                backgroundColor: pressed ? "white" : "#FF6C00",
-                borderColor: pressed ? "#FF6C00" : "white",
-              },
-              styles.submitButton,
-            ]}
-            onPressIn={() => {
-              setIsPressed(true);
-            }}
-            onPressOut={() => {
-              setIsPressed(false);
-            }}
-          >
-            <Text
-              style={[
-                styles.submitButtonText,
-                { color: isPressed ? "#FF6C00" : "white" },
-              ]}
+            <Text style={styles.title}>Реєстрація</Text>
+            <KeyboardAvoidingView
+              behavior={Platform.OS == "ios" ? "padding" : "height"}
             >
-              Зареєстуватися
-            </Text>
-          </Pressable>
-          <Pressable style={styles.loginLink}>
-            <Text style={styles.loginLinkText}>Вже є акаунт? Увійти</Text>
-          </Pressable>
-        </View>
-      </ImageBackground>
-    </View>
+              <View style={styles.form}>
+                <TextInput
+                  value={state.login}
+                  onChangeText={(e) =>
+                    dispatch({ type: "input_login", login: e })
+                  }
+                  inputMode="text"
+                  style={[
+                    styles.input,
+                    {
+                      borderColor: isFocusedLogin ? "#FF6C00" : "#E8E8E8",
+                      backgroundColor: isFocusedLogin ? "white" : "#F6F6F6",
+                    },
+                  ]}
+                  placeholder="Логін"
+                  onFocus={() => setIsFocusedLogin(true)}
+                  onBlur={() => setIsFocusedLogin(false)}
+                />
+                <TextInput
+                  value={state.email}
+                  onChangeText={(e) =>
+                    dispatch({ type: "input_email", email: e })
+                  }
+                  style={[
+                    styles.input,
+                    {
+                      borderColor: isFocusedEmail ? "#FF6C00" : "#E8E8E8",
+                      backgroundColor: isFocusedEmail ? "white" : "#F6F6F6",
+                    },
+                  ]}
+                  inputMode="email"
+                  placeholder="Адреса електронної пошти"
+                  onFocus={() => setIsFocusedEmail(true)}
+                  onBlur={() => setIsFocusedEmail(false)}
+                />
+                <View>
+                  <TextInput
+                    value={state.password}
+                    onChangeText={(e) =>
+                      dispatch({ type: "input_password", password: e })
+                    }
+                    inputMode="text"
+                    style={[
+                      styles.input,
+                      {
+                        borderColor: isFocusedPass ? "#FF6C00" : "#E8E8E8",
+                        backgroundColor: isFocusedPass ? "white" : "#F6F6F6",
+                      },
+                    ]}
+                    placeholder="Пароль"
+                    secureTextEntry={true}
+                    onFocus={() => setIsFocusedPass(true)}
+                    onBlur={() => setIsFocusedPass(false)}
+                  />
+                  <Pressable style={styles.passwordButton}>
+                    <Text style={styles.passwordButtonText}>Показати</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </KeyboardAvoidingView>
+            <Pressable
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed ? "white" : "#FF6C00",
+                  borderColor: pressed ? "#FF6C00" : "white",
+                },
+                styles.submitButton,
+              ]}
+              onPressIn={() => {
+                setIsPressed(true);
+              }}
+              onPressOut={() => {
+                setIsPressed(false);
+              }}
+              onPress={onReg}
+            >
+              <Text
+                style={[
+                  styles.submitButtonText,
+                  { color: isPressed ? "#FF6C00" : "white" },
+                ]}
+              >
+                Зареєстуватися
+              </Text>
+            </Pressable>
+            <Pressable style={styles.loginLink}>
+              <Text style={styles.loginLinkText}>Вже є акаунт? Увійти</Text>
+            </Pressable>
+          </View>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
